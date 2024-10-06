92e4731ec0517ac7e44def6d48214c366562c301
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2024-10-06 16:26:36 -0300

    feat: Add recognition module to INSTALLED_APPS and configure media settings
    - Add "recognition" to the INSTALLED_APPS list in settings.py
    - Configure MEDIA_URL and MEDIA_ROOT in settings.py
    - Update urls.py to include recognition.urls for API endpoints
    - Add static() function to urlpatterns for serving media files

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "recognition",
 ]
 
 MIDDLEWARE = [
@@ -132,3 +133,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
