b24874fa29ea498a48fed1dcd9f9c726beb28bc2
Samara Marques <samara.souza@scicrop.com>
2024-10-15 22:17:35 -0300

    refact: recognition models

--- backend/project/recognition/models.py
+++ backend/project/recognition/models.py
@@ -5,8 +5,10 @@ from django.db import models
 
 class User(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    nome = models.CharField(max_length=100)
-    imagem = models.ImageField(upload_to="users/")
+    name = models.CharField(max_length=100)
+    access_level = models.CharField()
+    email = models.CharField()
+    image = models.ImageField(upload_to="users/")
 
     class Meta:
         db_table = 'recognition_user'
