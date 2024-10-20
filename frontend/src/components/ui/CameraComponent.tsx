03142f64185c1e16c8410ca32981121a0241c1f3
LeoPDA <leonardo.arouchejk@gmail.com>
2024-10-20 20:35:18 -0300

    att frontend

--- frontend/src/components/ui/CameraComponent.tsx
+++ frontend/src/components/ui/CameraComponent.tsx
@@ -47,10 +47,10 @@ const CameraComponent: React.FC = () => {
   // };
 
   return (
-    <div>
-      <video ref={videoRef} autoPlay width="100%" height="480" />
+    <div >
+      <video ref={videoRef} autoPlay width="680" height="480" />
       <button onClick={captureImage}>Capturar Imagem</button>
-      <canvas ref={canvasRef} width="100%" height="480" style={{ display: 'none' }} />
+      <canvas ref={canvasRef} width="680" height="480" style={{ display: 'none' }} />
       <button onClick={startCamera}>Iniciar Câmera</button>
     </div>
   );
