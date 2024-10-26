9a9aba442278ac4b49faef6402e12a852bb9c313
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2024-10-26 15:19:32 -0300

    feat: update email field in User model

--- backend/project/recognition/models.py
+++ backend/project/recognition/models.py
@@ -6,9 +6,9 @@ from django.db import models
 class User(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
-    access_level = models.CharField()
-    email = models.CharField()
-    image = models.ImageField(upload_to="users/")
+    access_level = models.CharField(null=False, blank=False)
+    email = models.EmailField()
+    image = models.ImageField(upload_to="users/", null=False, blank=False)
 
     class Meta:
-        db_table = 'recognition_user'
+        db_table = "recognition_user"
