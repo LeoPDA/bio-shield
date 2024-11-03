0369acc5d620000060cd548593e7aff7459c9cc0
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2024-11-03 19:40:38 -0300

    feat: Refactor Nivel1 component to use DataTable and fetch toxin data from API

--- frontend/src/pages/Nivel1.tsx
+++ frontend/src/pages/Nivel1.tsx
@@ -1,35 +1,33 @@
-import { Table, TableHeader, TableBody, TableRow, TableCell } from '../components/ui/table';
+import { columns } from "@/components/data-table/columns";
+import { DataTable } from "@/components/data-table/data-table";
+import { Toxin } from "@/schemas/toxin-schema";
+import { useEffect, useState } from "react";
 
-const data = [
-    { id: 1, name: 'Item 1', value: 'Valor 1' },
-    { id: 2, name: 'Item 2', value: 'Valor 2' },
-    { id: 3, name: 'Item 3', value: 'Valor 3' },
-];// Importando os componentes de tabela do Shadcn
+async function getData(): Promise<Toxin[]> {
+  const response = await fetch("http://localhost:8000/api/toxins");
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return (await response.json()) as Toxin[];
+}
 
 const Nivel1 = () => {
-    return (
-        <div>
-            <h1>Camada 1</h1>
-            <Table>
-                <TableHeader>
-                    <TableRow>
-                        <TableCell>ID</TableCell>
-                        <TableCell>Nome</TableCell>
-                        <TableCell>Valor</TableCell>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    {data.map((item) => (
-                        <TableRow key={item.id}>
-                            <TableCell>{item.id}</TableCell>
-                            <TableCell>{item.name}</TableCell>
-                            <TableCell>{item.value}</TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </div>
-    );
+  const [data, setData] = useState<Toxin[]>([]);
+
+  useEffect(() => {
+    getData().then((data) => setData(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Camada 1</h1>
+      <div className="container mx-auto py-10">
+        <DataTable data={data} columns={columns} />
+      </div>
+    </div>
+  );
 };
 
 export default Nivel1;
