c010fcca36be87ac48929013bd6443e954528fc7
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2024-11-03 19:40:21 -0300

    feat: Add classification data with icons for different levels

---
+++ frontend/src/data/classification.ts
@@ -0,0 +1,23 @@
+import {
+  ArrowDownIcon,
+  ArrowRightIcon,
+  ArrowUpIcon,
+} from "@radix-ui/react-icons";
+
+export const classification = [
+  {
+    label: "Baixa",
+    value: "BAIXA",
+    icon: ArrowDownIcon,
+  },
+  {
+    label: "Média",
+    value: "MEDIA",
+    icon: ArrowRightIcon,
+  },
+  {
+    label: "Alta",
+    value: "ALTA",
+    icon: ArrowUpIcon,
+  },
+];
