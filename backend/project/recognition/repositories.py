3e9bd65f0b065aad2c5d084d0b5552166a2992f7
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2024-11-04 22:49:09 -0300

    feat: Remove UserRepository; add UserSerializer for user data handling in authentication views

--- backend/project/recognition/repositories.py
+++
@@ -1,7 +0,0 @@
-from project.recognition.models import User
-
-
-class UserRepository:
-    @staticmethod
-    def get_all_users():
-        return User.objects.all()
--- backend/project/recognition/views.py
+++ backend/project/recognition/views.py
@@ -2,7 +2,9 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from .serializers import UserSerializer
 from .services import UserService
+
 
 class AuthenticateUserView(APIView):
     def post(self, request):
@@ -10,32 +12,32 @@ class AuthenticateUserView(APIView):
             if not (image_sent := request.FILES.get("image")):
                 return Response(
                     {"error": "image não fornecida."},
-                    status=status.HTTP_400_BAD_REQUEST
+                    status=status.HTTP_400_BAD_REQUEST,
                 )
 
-            if user := UserService.authenticate_user(image_sent):
-                    # Verificar se o nível de acesso é um dos permitidos
-                return (
-                    Response(
-                        {"name": user.name, "access_level": user.access_level},
-                        status=status.HTTP_200_OK,
-                    )
-                    if user.access_level in [1, 2, 3]
-                    else Response(
-                        {"error": "Nível de acesso não autorizado."},
-                        status=status.HTTP_403_FORBIDDEN,
-                    )
-                )
-            else:
+            if not (user := UserService.authenticate_user(image_sent)):
                 # Caso nenhum usuário tenha sido encontrado
                 return Response(
                     {"error": "Usuário não encontrado."},
-                    status=status.HTTP_404_NOT_FOUND
+                    status=status.HTTP_404_NOT_FOUND,
                 )
+            user_data = UserSerializer(user).data
+
+            return (
+                Response(
+                    user_data,
+                    status=status.HTTP_200_OK,
+                )
+                if user.access_level in [1, 2, 3]
+                else Response(
+                    {"error": "Nível de acesso não autorizado."},
+                    status=status.HTTP_403_FORBIDDEN,
+                )
+            )
         except Exception as e:
             return Response(
                 {"error": f"Erro durante autenticação: {str(e)}"},
-                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
             )
 
 
@@ -60,13 +62,17 @@ class RegisterUserView(APIView):
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR,
             )
 
+
 class UploadImageView(APIView):
     def post(self, request, user_id):
         try:
             image = request.FILES.get("image")
 
             if not image:
-                return Response({"error": "Nenhuma imagem enviada."}, status=status.HTTP_400_BAD_REQUEST)
+                return Response(
+                    {"error": "Nenhuma imagem enviada."},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
 
             dados, erros = UserService.upload_image_user(user_id, image)
             if erros:
