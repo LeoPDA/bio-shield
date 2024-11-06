0ab38fffc41935bc58c25304b96a0ee7b3eaf4e6
LeoPDA <leonardo.arouchejk@gmail.com>
2024-11-05 22:13:29 -0300

    feat: Update frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import {
   Routes,
 } from "react-router-dom";
 //import Home from './pages/Home';
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import Navbar from "./components/ui/navbar";
 import { AuthProvider, useAuth } from "./contexts/AuthContext"; // Importe o contexto de autenticação
 import Auth from "./pages/Auth";
@@ -14,27 +14,35 @@ import Nivel1 from "./pages/Nivel1";
 import Nivel2 from "./pages/Nivel2";
 import Nivel3 from "./pages/Nivel3";
 import Toxin from "./pages/Toxin";
+import Swal from "sweetalert2";
 
 interface PrivateRouteProps {
   element: ReactNode; // ou React.ReactElement se preferir
   requiredLevel: number;
 }
 
-const PrivateRoute: React.FC<PrivateRouteProps> = ({
-  element,
-  requiredLevel,
-}) => {
+const PrivateRoute: React.FC<PrivateRouteProps> = ({ element, requiredLevel }) => {
   const { user, loading } = useAuth();
+
+  useEffect(() => {
+    if (user && user.access_level < requiredLevel) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Acesso negado',
+        text: 'Você não tem permissão para acessar esta página.',
+      });
+    }
+  }, [user, requiredLevel]);
 
   if (loading) {
     return <div>Carregando...</div>;
   }
 
-  return user && user.access_level >= requiredLevel ? (
-    <>{element}</>
-  ) : (
-    <Navigate to="/auth" />
-  );
+  if (!user || user.access_level < requiredLevel) {
+    return <Navigate to="/auth" />;
+  }
+
+  return <>{element}</>;
 };
 
 const AuthRoute: React.FC<{ element: ReactNode }> = ({ element }) => {
